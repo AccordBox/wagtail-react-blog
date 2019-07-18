cf13a6fa073e9df716bb79d53b7e72b5fe740302
Michaelyin <hgyinjian@gmail.com>
2019-07-18 17:03:10 +0800

    add ALLOWED_HOSTS

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -10,6 +10,12 @@ SECRET_KEY = '5+f#!xn=hj^u#=cr9@pz@@5cf7bqf0ymy=8uyfpx_zvxpght3='
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+ADMINS = (
+    ('Michael Yin', 'admin@michaelyin.info'),
+)
+
+ALLOWED_HOSTS = ['*']
+
 try:
     from .local import *
 except ImportError:
