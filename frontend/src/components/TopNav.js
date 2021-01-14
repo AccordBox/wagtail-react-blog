be34c345adb80caa1bce8d4e29ed1d6037a1cd85
MichaelYin <hgyinjian@gmail.com>
2021-01-14 11:37:07 +0800

    add book link

--- frontend/src/components/TopNav.js
+++ frontend/src/components/TopNav.js
@@ -11,8 +11,9 @@ class TopNav extends React.Component {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Nav.Link target='_blank' href={process.env.REACT_APP_STORYBOOK_NAV_LINK}>StoryBook</Nav.Link>
-              <Nav.Link target='_blank' href={process.env.REACT_APP_API_WAGTAIL_NAV_LINK}>Wagtail Admin</Nav.Link>
+              <Nav.Link target="_blank" href={process.env.REACT_APP_STORYBOOK_NAV_LINK}>StoryBook</Nav.Link>
+              <Nav.Link target="_blank" href={process.env.REACT_APP_API_WAGTAIL_NAV_LINK}>Wagtail Admin</Nav.Link>
+              <Nav.Link target="_blank" href="https://leanpub.com/react-wagtail">Ebook</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
