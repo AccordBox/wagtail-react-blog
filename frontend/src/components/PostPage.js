6504e2ccbd5658401f953a349c63868da3824575
kenichi miyata <miyataken999@bpmboxes.com>
2021-08-05 20:25:48 +0000

    'sss'

--- frontend/src/components/PostPage.js
+++ frontend/src/components/PostPage.js
@@ -8,7 +8,7 @@ import { PostDetail } from "./PostDetail";
 class PostPage extends React.Component {
   render() {
     return (
-      <div>
+      <div>ssssssssssssssssssss
         <TopNav/>
         <Container>
           <Row>
